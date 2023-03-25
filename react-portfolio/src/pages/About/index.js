007ee4c95a66630b48ec2726c762c53dbbcdb3e7
Sarah Jacobs <sgjacobs14@gmail.com>
2023-03-25 16:28:05 -0400

    updated pages

--- react-portfolio/src/pages/About/index.js
+++ react-portfolio/src/pages/About/index.js
@@ -4,6 +4,8 @@ function About () {
     return (
         <section>
             <h2>About</h2>
+            <h3>Sarah Jacobs</h3>
+            <p>Sarah Jacobs is a Software Engineer with a background in communication and digital media. She is currently seeking roles in Software Engineering, Wed Development, and more.</p>
         </section>
     )
 }
--- react-portfolio/src/pages/Contact/index.js
+++ react-portfolio/src/pages/Contact/index.js
@@ -4,6 +4,8 @@ function Contact () {
     return (
         <section>
             <h2>Contact</h2>
+            <p>Have a question or comment? Contact Sarah here:</p>
+            <br/>
         </section>
     )
 }
